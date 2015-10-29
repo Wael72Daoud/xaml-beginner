628090d376e1a1245368b544c8e262454b4b4822
Wael72Daoud <wael72daoud@gmail.com>
2015-10-28 23:49:47 -0700

    the third assignment

---
+++ RestaurantManager.UniversalWindows/Models/DataManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.UniversalWindows.Models
+{
+    class DataManager
+    {
+
+        public ObservableCollection<string> OrderItems { set; get; }
+        public List<string> MenueItems { set; get; }
+        public List<string> CurrentlySelectedMenuItems { set; get; }
+
+        public DataManager()
+        {
+
+            this.OrderItems = new ObservableCollection<string>(
+                new List<string>
+                {
+                    "Steak, Chicken, Peas",
+                    "Rice, Chicken",
+                    "Hummus, Pita"
+                }
+                );
+
+            this.MenueItems = new List<string>(
+                new List<string>
+                {
+                    "Steak",
+                    "Chicken",
+                    "Peas",
+                    "Rice",
+                    "Humus",
+                    "Pita"
+                   
+                }
+                );
+
+            this.CurrentlySelectedMenuItems = new List<string>(
+                new List<string>
+                {
+                    "Rice",
+                    "Pita"
+
+
+                });
+        }
+    }
+}
